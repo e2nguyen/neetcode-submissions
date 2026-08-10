0599bc7e0e8ba86036cf424273603f608753ce15
Erica Nguyen <29549301+e2nguyen@users.noreply.github.com>
2026-08-09 18:50:44 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-28.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        # nums_map = {-1: 0, 5: 1, -3: 2}
+        for index in range(len(nums)):
+            complement = target - nums[index]
+            if complement in nums_map:
+                print([nums_map[complement], index])
+                return [nums_map[complement], index]
+            print(nums_map)
+            nums_map[nums[index]] = index
